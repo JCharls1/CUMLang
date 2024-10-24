6377567144ab89d860382f58da098e67b10a68b1
JCharls1 <garciajohncharles33@gmail.com>
2024-10-24 18:34:27 +0800

    quick update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import TextEditor from "./TextEditor";
 import Documentation from "./Documentation";
+import Reminder from "./Reminder";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <h1 className="text-silver-shit text-3xl font-Arial">CUM Lang</h1>
         </div>
         <TextEditor></TextEditor>
+        <Reminder></Reminder>
         <Documentation></Documentation>
       </div>
     </>
